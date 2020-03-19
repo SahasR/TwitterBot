64dd733b9d7018642ad2cca663c99a31f223f68e
Sahas Renuja Gunasekara <56167123+SahasR@users.noreply.github.com>
2020-03-19 21:33:48 +0530

    Made it a bit more User Friendly
    General Bug fixes and making it less of a pain in the a** to use
    You know the deal

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import tweepy
-import time
 import requests
-from bs4 import BeautifulSoup
-# import time
+from bs4 import BeautifulSoup, SoupStrainer
 from datetime import datetime
+import time
+import httplib2
+
 
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
@@ -19,39 +20,61 @@ print('this is my twitter bot', flush=True)
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+# print(api)
 
 FILE_NAME = 'last_seen_id.txt'
 
 CountryName = 'Sri Lanka'
 
-def begin_dissection(res):
-    global Tot
-    global IPM
-    Tot = res[12:15]
-    # print(Tot)
-    IPM = res[-4:-1]
-    # print(IPM)
+
+def get_sitrep():
+    sitrep_found = False
+    print("Getting Data from Epidemiology Unit Sri Lanka...")
+    epid_date1 = time.strftime("%d-%m_10")
+    epid_date2 = time.strftime("%d-%m-20%y_10")
+    now = datetime.now()
+    current_time = now.strftime("%m/%d/%Y")
+    http = httplib2.Http()
+    pre_req = [epid_date1, "sl", "en"]
+    pre_req2 = [epid_date2,"sl","en"]
+    status, response = http.request('http://www.epid.gov.lk/web/index.php?option=com_content&view=article&id=225&lang=en')
+    for link in BeautifulSoup(response, parse_only=SoupStrainer('a'),features="html.parser"):
+        if link.has_attr('href'):
+            if all(c in link['href'] for c in pre_req):
+                print("Got the SitRep....")
+                sitrep_pdf = link['href']
+                sitrep_found = True
+            elif all(c in link['href'] for c in pre_req2):
+                print("Got the SitRep....")
+                sitrep_pdf = link['href']
+                sitrep_found = True
+    if sitrep_found == True:
+        sitrep_pdf = "http://www.epid.gov.lk" + sitrep_pdf
+        print(sitrep_pdf)
+        ReplyString = f"Today is {current_time},\nHere is the Situational Report by the Epidemiology Unit of Sri Lanka\n{sitrep_pdf}\n#LKA #SriLanka #Coronavirus #COVID2019 #COVID19"
+        print(ReplyString)
+        corona_prompt = input("Is the User pleased with the Reply String?(Y/N): ").lower()
+        if corona_prompt == "y":
+            print("Posting on @sahasbot...")
+            api.update_status(ReplyString)
+    else:
+        print("Situational Report Not Found, Perhaps it is not uploaded yet...")
+
 
 def get_corona():
     print("Fetching Corona Stats...")
-    URL = 'https://www.worldometers.info/coronavirus/'
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    # soup = BeautifulSoup(URL)
-    countries = soup.find_all('tr')
-    # t = time.localtime()
-    # current_time = time.strftime("%H:%M:%S", t)
     now = datetime.now()
     current_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    for country in countries:
-        str = country.get_text()
-        if CountryName in str:
-            res = str
-            # print(str)
-            # print(len(str))
-            begin_dissection(res)
-    ReplyString = 'Sri Lanka\nNo. Of Covid-19 Patients: ' + Tot + '\nNo. Of Patients Per Million Of Population: ' + IPM +'\n' + current_time +'\n#LKA #SriLanka\nFrom worldometers.info'
+    URL = 'http://hpb.health.gov.lk/api/get-current-statistical'
+    page = requests.get(URL)
+    json_object = page.json()
+    new_cases = json_object["data"]["local_new_cases"]
+    Tot = json_object["data"]["local_total_cases"]
+    Rec = json_object["data"]["local_recovered"]
+    global_new = json_object["data"]["global_new_cases"]
+    ReplyString = 'Sri Lanka\nTotal No. Of Covid-19 Patients:' + str(Tot) + '\nNew Cases:' + str(new_cases) + '\nNo. Of Patients Recovered:' + str(Rec) + '\nNew Global Cases:' + str(global_new) +'\n' + current_time + '\n#LKA #SriLanka #Coronavirus #COVID2019 #COVID19\nFrom hpb.health.gov.lk'
     print(ReplyString)
+    print(len(ReplyString))
     corona_prompt = input("Is the User pleased with the Reply String?(Y/N): ").lower()
     if corona_prompt == "y":
         print("Posting on @sahasbot...")
@@ -105,17 +128,31 @@ def reply_to_tweets():
             print('Found request for weather..', flush=True)
             print('Calling Weather Function..', flush=True)
             get_temperature()
-            ReplyString = '@' + mention.user.screen_name + '\nCurrent Temperature: ' + temp + ' Celcius\nCurrent Humidity: ' + humidity + '%\nWind Speed: ' +wind + 'meters per second\nDesc: ' + desc
-            print("Issuing Status Update....")
-            api.update_status(ReplyString)
+            ReplyString = f"Current Temperature: {temp} Celcius\nCurrent Humidity: {humidity}%\nWind Speed: {wind} meters per second\nDesc: {desc}\nhttps://twitter.com/{mention.user.screen_name}/status/{mention.id}"
+            # ReplyString = '@' + mention.user.screen_name + '\nCurrent Temperature: ' + temp + ' Celcius\nCurrent Humidity: ' + humidity + '%\nWind Speed: ' +wind + 'meters per second\nDesc: ' + desc +"\nhttps://twitter.com/<user_displayname>/status/<tweet_id>"
+            print(ReplyString)
+            user_prompt = input("Is the User pleased with the Reply String?(Y/N): ").lower()
+            if user_prompt == "y":
+                print("Posting on @sahasbot...")
+                api.update_status(ReplyString)
 
 
 
 
+while True:
+    sitrep_found = False
+    input_option = input("T: Get Temperature\nR: Reply To Tweets\nC: Get Corono Stats\nS: Get Sitrep\nX: Exit\nWhat Needs to be Done?: ")
+    if input_option.lower() == "t":
+        reply_to_tweets()
+    if input_option.lower() == "r":
+        reply_to_tweets()
+    if input_option.lower() == "c":
+        get_corona()
+    if input_option.lower() == "s":
+        get_sitrep()
+    if input_option.lower() == "x":
+        exit()
 
-# get_temperature()
-# reply_to_tweets()
-get_corona()
 
 
 
