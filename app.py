dff16ec837f4722d616c1b3b46d8b9fee5d3e3bc
Sahas Renuja Gunasekara <56167123+SahasR@users.noreply.github.com>
2020-04-13 17:38:42 +0530

    MatPlotLib extension
    Added an extension based on MatPlotLib to create a graph using the latest CoronaVirus Updates

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup, SoupStrainer
 from datetime import datetime
 import time
 import httplib2
+import matplotlib.pyplot as plt
+import os
+
 
 
 CONSUMER_KEY = ""
@@ -23,9 +26,9 @@ api = tweepy.API(auth)
 # print(api)
 
 FILE_NAME = 'last_seen_id.txt'
-
 CountryName = 'Sri Lanka'
-
+Date = []
+Numbers = []
 
 def get_sitrep():
     sitrep_found = False
@@ -60,6 +63,64 @@ def get_sitrep():
     else:
         print("Situational Report Not Found, Perhaps it is not uploaded yet...")
 
+def initialise_data():
+    global Date
+    global Numbers
+    global Average
+    corona_stats = open('temp.txt', 'r')
+    Date.clear()
+    Numbers.clear()
+    for line in corona_stats:
+        line = line.strip('\n')
+        Day, Number = line.split(" ")
+        Date.append(Day)
+        Numbers.append(int(Number))
+    print("Initialise Function Called")
+    # print(Date)
+    # print(Numbers)
+    corona_stats.close()
+    Average = sum(Numbers) / len(Numbers)
+    # print(Average)
+
+def setup_image():
+    found_today = False
+    now = datetime.now()
+    current_date = now.strftime("%d/%m")
+    initialise_data()
+    stored_total = sum(Numbers)
+    NewData = Tot - stored_total
+    Number_of_elements = len(Numbers)
+    # print(Numbers)
+    # print(Date)
+    UpdatedNumber = stored_total - Numbers[Number_of_elements-1]
+    UpdatedNumber = Tot - UpdatedNumber
+    for X in Date:
+        if X == current_date:
+            found_today = True
+    if found_today == False:
+        Date.append(current_date)
+        Numbers.append(NewData)
+        corona_stats = open('temp.txt', 'a')
+        corona_stats.write(f'{current_date} {str(NewData)}\n')
+        corona_stats.close()
+        initialise_data()
+    else:
+        x = 0
+        for day in Date:
+           if day == current_date:
+               Numbers[x] = UpdatedNumber
+           x += 1
+        corona_stats = open('temp.txt', 'w')
+        x = 0
+        for each in Date:
+            corona_stats.write(f'{Date[x]} {str(Numbers[x])}\n')
+            x += 1
+    corona_stats.close()
+    initialise_data()
+    print(Numbers)
+    print(Date)
+
+
 
 def get_corona():
     print("Fetching Corona Stats...")
@@ -68,17 +129,39 @@ def get_corona():
     URL = 'http://hpb.health.gov.lk/api/get-current-statistical'
     page = requests.get(URL)
     json_object = page.json()
+    global Tot
     new_cases = json_object["data"]["local_new_cases"]
     Tot = json_object["data"]["local_total_cases"]
     Rec = json_object["data"]["local_recovered"]
     global_new = json_object["data"]["global_new_cases"]
-    ReplyString = 'Sri Lanka\nTotal No. Of Covid-19 Patients:' + str(Tot) + '\nNew Cases:' + str(new_cases) + '\nNo. Of Patients Recovered:' + str(Rec) + '\nNew Global Cases:' + str(global_new) +'\n' + current_time + '\n#LKA #SriLanka #Coronavirus #COVID2019 #COVID19\nFrom hpb.health.gov.lk'
+    local_deaths = json_object["data"]["local_deaths"]
+    setup_image()
+    plt.plot(Date, Numbers, color="#6C969D")
+    plt.axhline(y=Average, linestyle="--", color='#FF3A20', label='Average Cases per Day')
+    plt.legend(numpoints=1)
+    ax = plt.gca()
+    plt.grid()
+    ax.set_facecolor('#dcdde1')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    plt.ylabel('Number of Patients')
+    # plt.xlabel('Day')
+    plt.xticks(rotation=90)
+    plt.title('Pattern of Discovered Patients')
+    # plt.show()
+    plt.savefig('corona_plot.png')
+    ReplyString = 'Sri Lanka\nTotal No. Of Covid-19 Patients:' + str(Tot) + '\nNew Cases:' + str(new_cases) + '\nNo. Of Patients Recovered:' + str(Rec) +'\nLocal Deaths:' + str(local_deaths) +'\nNew Global Cases:' + str(global_new) +'\n' + current_time + '\n#LKA #SriLanka #Coronavirus #COVID2019 #COVID19\nFrom hpb.health.gov.lk'
     print(ReplyString)
     print(len(ReplyString))
     corona_prompt = input("Is the User pleased with the Reply String?(Y/N): ").lower()
     if corona_prompt == "y":
         print("Posting on @sahasbot...")
-        api.update_status(ReplyString)
+        api.update_with_media('corona_plot.png', status=ReplyString)
+    data_file = 'corona_plot.png'
+    os.remove(data_file)
+    plt.close()
 
 def get_temperature():
     r = requests.get('http://api.openweathermap.org/data/2.5/weather?id=1248991&appid=c5cdc721216339b2b9da9d05cf535617')
